3e5a22e00cb1afd9c7523c3cb6827a28b5f87dff
Ruslan Sivitsky <rsivitsky@gmail.com>
2015-12-10 01:30:46 +0300

    security

--- src/main/java/com/sivitsky/ddr/LoginController.java
+++ src/main/java/com/sivitsky/ddr/LoginController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String loginPage(Model model){
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String loginPageGet(Model model){
         return "login";
+    }
+
+    @RequestMapping(value="j_spring_security_check", method = RequestMethod.POST)
+    public String loginPagePost(Model model){
+        return "index";
     }
 }
